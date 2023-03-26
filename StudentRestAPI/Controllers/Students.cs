b478e1b178d8859e6cd35a35b9d987c2bb8d5e04
Diego Murillo <diegomurilloferrer@gmail.com>
2023-03-25 21:09:34 -0500

    Add project files.

---
+++ StudentRestAPI/Controllers/Students.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Students : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents ()
+        {
+            string studentName = "Jose";
+            return studentName;
+        }
+
+    }
+}
---
+++ StudentRestAPI/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentRestAPI.Models
+{
+    public class Student
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
